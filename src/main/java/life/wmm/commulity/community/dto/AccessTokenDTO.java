9cfa7fd300cdf0c42c3429de47920447eabdce9a
w-mm <2498408614@qq.com>
2020-07-25 16:16:06 +0800

    6ci

---
+++ src/main/java/life/wmm/commulity/community/dto/AccessTokenDTO.java
@@ -0,0 +1,18 @@
+package life.wmm.commulity.community.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AccessTokenDTO {
+    private String client_id;
+    private String client_secret;
+    private String code;
+    private String redirect_uri;
+    private String state;
+}
---
+++ src/main/java/life/wmm/commulity/community/dto/GithubUser.java
@@ -0,0 +1,16 @@
+package life.wmm.commulity.community.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class GithubUser {
+    private Long id;
+    private String name;
+    private String bio;
+}
